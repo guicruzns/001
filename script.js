e8a9d380bd308e38f010d37f56d52e7d9e7cf44d
guicruzns <guilherme.ns.cruz@gmail.com>
2021-01-27 18:51:48 -0300

    finalização

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ function Contar(){
     var start = document.getElementById('start')
     var step = document.getElementById('step')
     var final = document.getElementById("final")
-    if (Number(start.value)> 0 && Number(final.value)> 0 && Number(step.value) != 0){
+    if (Number(start.value)> 0 && Number(final.value)> 0 && Number(step.value) > 0){
         var nstart = Number(start.value)
         var nstep = Number(step.value)
         var nfinal = Number(final.value)
         var res = document.querySelector('div#nb')
-        if(nstart < nfinal && nstart != nfinal){
-            while(nstart<=nfinal){
-                nstart + nstep
-                res.innerHTML = `${nstart}`
-            }
-        }
+        res.innerHTML = "..."
+        if(nstart < nfinal){
+
+            for(var i = nstart;i <= nfinal;i += nstep){res.innerHTML += `\u{1F449} ${i}`}res.innerHTML += `\u{1F449} \u{1f3c1}`}
+        else if(nstart > nfinal){
+
+            for(var c = nstart;c >= nfinal;c -= nstep){res.innerHTML += `\u{1F449} ${c}`} res.innerHTML += `\u{1F449} \u{1f3c1}`}
+        else if(nstart == nfinal){res.innerHTML += `\u{1F449} ${nstart} \u{1f3c1}`}
 }
 }
